bbf9c25d7664682c52c1d036ace66a9d53b4f07c
Amit Vishwakarma <amitvishwa789@gmail.com>
2026-07-26 21:09:24 +0530

    updated description

--- bosscoderacademy/src/main/java/module/one/bit_manipulation/assignment/_1SingleNumberI.java
+++ bosscoderacademy/src/main/java/module/one/bit_manipulation/assignment/_1SingleNumberI.java
@@ -6,21 +6,21 @@ import java.util.HashMap;
 
 /**
  * Single Number 1:
- * <p>
+ *
  * Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
  * You must implement a solution with a linear runtime complexity and use only constant extra space.
- * <p>
+ *
  * Input 1: nums = [2,2,1]
  * Output 1: 1
  * Explanation 1: 1 is present only once.
- * <p>
+ *
  * Input 2: nums = [4,1,2,1,2]
  * Output 2: 4
- * <p>
+ *
  * Constraints:
  * 1 <= nums.length <= 3*104
  * -3*104 <= nums[i] <= 3*104
- * <p>
+ *
  * Approaches:
  * - There are various approaches to solve this problem, they are as follows:
  * 1. Bruteforce using nested loop - Time complexity: O(N^2), Space complexity: O(1)
