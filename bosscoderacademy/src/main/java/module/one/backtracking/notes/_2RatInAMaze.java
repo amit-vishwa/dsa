ae4f12e65f68c30854cee530e6376a864351cdc9
Amit Vishwakarma <amitvishwa789@gmail.com>
2026-08-18 08:25:01 +0530

    updated description

--- bosscoderacademy/src/main/java/module/one/backtracking/notes/_2RatInAMaze.java
+++ bosscoderacademy/src/main/java/module/one/backtracking/notes/_2RatInAMaze.java
@@ -2,16 +2,16 @@ package module.one.backtracking.notes;
 
 /**
  * Rat in a Maze: [Equivalent Leetcode 980. Unique Paths III]
- * <p>
+ *
  * Consider a rat placed at (0, 0) in a square matrix of order N * N. It has to reach the destination at (N - 1, N - 1). Find all
  * possible paths that the rat can take to reach from source to destination. The directions in which the rat can move are 'U'(up),
  * 'D'(down), 'L' (left), and 'R' (right). Value 0 at a cell in the matrix represents that it is blocked and the rat cannot move to
  * it while value 1 at a cell in the matrix represents that the rat can travel through it.
- * <p>
+ *
  * Example
  * Input: N = 4 m[][] = {{1, 0, 0, 0}, {1, 1, 0, 1}, {1, 1, 0, 0}, {0, 1, 1, 1}}
  * Output: DDRDRR DRDDRR
- * <p>
+ *
  * Approach:
  * - The approach is quite simple for this problem.
  * - We just have to visit only the cell which are in maze and marked as can be visited i.e. 1 here.
